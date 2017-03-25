899d486867f374eebb91db12f7e06f92947db61e
JAKE SCHRAMM <schramm@nike.cs.uga.edu>
2017-03-25 18:13:04 -0400

    brand new editor, added scrolling, new displayFile function

--- Editor.cpp
+++ Editor.cpp
@@ -1,113 +1,139 @@
-#include <string>
 #include <ncurses.h>
-#include "Editor.h"
+#include <form.h>
+#include "e.h"
+#include <string>
+#include <fcntl.h> //low level I/O
+#include <unistd.h> //close()
+#include <string.h>
 
-Editor::Editor(char * fileName) { 
+using std::string;
+
+e::e(const char * filename)
+{
+  this->filename = filename; 
 
   initscr();
   noecho();
-  getmaxyx(stdscr, yMax, xMax);
   
-  container = newwin(yMax - 4, xMax-2, 1, 0); // used for visible border
-  editArea = newwin(yMax - 6, xMax-6, 2, 2); // limits in which user can write text (invisible border)
+  mvaddstr(1, 0, "F1: Menu");
+  mvaddstr(1, COLS/2 - 2, "CSCI 1730 Text Editor!");
+  mvaddstr(LINES-2, 0, filename);
   
-  wborder(container, '|', '|', '_', '_', ' ',' ', '|', '|'); 
-  keypad(editArea, TRUE);
-  scrollok(editArea, TRUE);
 
-  mvwprintw(stdscr, 0, 0, "F1: Menu");
-  mvwprintw(stdscr, 0, xMax/2 - 10, "CSCI 1730 Editor!");
-  mvwprintw(stdscr, yMax-3, 0, fileName); // print .txt name at bottom left 
+  /* Create fields (field[0] is the text editing space) */
+  field[0] = new_field(LINES-8, COLS-8, 1, 1, 0, 0); //height, width, starty, startx, number of offscreen rows and number of additional working buffers.
+  field[1] = NULL; //the last element in array field must be NULL   
+  field_opts_off(field[0], O_AUTOSKIP | O_STATIC); //makes the field growable 
   
-  wmove(editArea, 0, 0);
-  currY = 0;
-  currX = 0;
+  /* Create the form */
+  form = new_form(field);
+  form_opts_off(form, O_NL_OVERLOAD); //gets rid of 'special behavior' when creating a new line using the form driver below 
 
+  /* Create the window (border) */
+  win = newwin(LINES-4, COLS-2, 2, 0); //where you want the border
+  keypad(win, TRUE);
+  wborder(win, '|', '|', '_', '_', ' ',' ', '|', '|');
+  
+  set_form_win(form, win);
+  set_form_sub(form, derwin(win, LINES-6, COLS-6, 1, 1)); //must be smaller/inside win
+ 
+  /* Order is very important here: */
   refresh();
-  wrefresh(container);
-  wrefresh(editArea);
-}
-
-void Editor::displayFile(char * file) {
-
-  //print contents of file into editArea
-
-}
-
-void Editor::handleKeyInput() {
-  int key;
+  post_form(form);
+  wrefresh(win);
+  refresh();
   
-  int maxEdX;
-  int maxEdY;
-  getmaxyx(editArea, maxEdY, maxEdX);
+  form_driver(form, REQ_INS_CHAR); //so that cursor starts inside the form window
+}
 
-  while(1) {
-    wmove(editArea, currY, currX);
-    key = wgetch(editArea); //important: wgetch, not just getch()
 
-    switch(key) {
-      
-      /* CASES */
-    case KEY_UP: //remember to handle scrolling
-      if(currY > 0)
-	currY--;
-      break;
+void e::handleKeyInput() {
+  bool listening = true;
+  while(listening) {
+    int ch = wgetch(win); //notice: keypad is on WINDOW
+   
+    switch(ch)
+      {
+      case KEY_DOWN:
+	form_driver(form, REQ_DOWN_CHAR);
+	break;
+      case KEY_UP:
+	form_driver(form, REQ_UP_CHAR);
+	break;
+      case KEY_RIGHT:
+	form_driver(form, REQ_NEXT_CHAR);
+	break;
+      case KEY_LEFT:
+	form_driver(form, REQ_PREV_CHAR);
+	break;
+      case 10: //ENTER                          
+	form_driver(form, REQ_NEW_LINE);
+	break;
+      case KEY_BACKSPACE:                                                                                                                                                     
+	form_driver(form, REQ_DEL_PREV);
+     	break;
+      case KEY_F(1):
+	openMenu(); //does nothing yet...leave loop
+	listening = false;
+	break;
+      default:
+	waddch(win, ch);
+	form_driver(form, REQ_NEXT_CHAR);
+	break;
+      } //switch
+  }//while	
+  
+}
 
-    case KEY_DOWN:
-      if(currY < maxEdY - 1)
-	currY++;
-      break;
 
-    case KEY_LEFT:
-      if(currX > 0)
-	currX--;
-      break;
+void e::openMenu() //when user hits F1
+{
+  
+}
 
-    case KEY_RIGHT:
-      if(currX < maxEdX - 1)
-	currX++;
-      break;
+void displayError() 
+{
 
-    case 127: //BACKSPACE
-      if(currX > 0)   
-	mvwdelch(editArea, currY, --currX);
-      break;
-      
-    case 10: //ENTER
-      waddch(editArea, '\n');
-      currY++;
-      currX = 0;
-      break;
+}
 
-    default:
-      if(currX < maxEdX) {
-	waddch(editArea, key);
-	currX++;
-      }
-      
-      else { //at end of line
-	if(currY < maxEdY) { 
-	  mvwaddch(editArea, ++currY, currX = 0, key);
-      }
-      break;
-    }
 
-    wrefresh(editArea);
-  }//while
+void e::displayFile(string buff, int lineNum) 
+{   
+  form_driver(form, REQ_BEG_FIELD); //go to very beginning of edit area
+  for(int i = 0; i < lineNum; i++) 
+    form_driver(form, REQ_DOWN_CHAR); //go down lineNum number of lines 
     
-  }
-  
+  waddstr(win, buff.c_str()); //add str at current pos
+}
 
+
+	
+void e::save(const char * filename)
+{
+
+}
+
+void e::saveAs(const char * filename)
+{
+  
+}
+
+void e::exit() 
+{
+  unpost_form(form);
+  free_form(form);
+  free_field(field[0]);
+  endwin();
+  //close(fd); 
 }
 
 
 
-int main(int argc, char * argv []) { //test
-  
-  Editor ed1{argv[1]};
-  ed1.handleKeyInput();
-  
-  endwin();
+int main(int argc, const char * argv[]) 
+{
+  e ed = e("newfile.txt"); //create the editor 
+  ed.handleKeyInput();
+  ed.exit();
   return 0;
-  
+
 }
--- Editor.h
+++ Editor.h
@@ -1,26 +1,33 @@
-#ifndef EDITOR_H
-#define EDITOR_H
+#ifndef E_H
+#define E_H
 
 #include <string>
 #include <ncurses.h>
 
-class Editor {
+using std::string;
 
- private: 
-  char * file; //the .txt file being edited 
-  WINDOW * container;
-  WINDOW * editArea;
-  int yMax;
-  int xMax;
-  int currY;
-  int currX;
+class e
+{
 
-  
- public: 
-  Editor(char *); // constructor, takes in a filename as a parameter (could be argv[1]) 
-  void handleKeyInput();
-  void openMenu(); // when user presses F1
-  void displayFile(char *); //display file contents in the edit area  
+ private:
+  WINDOW * win; //container 
+  FIELD * field [2]; //1st element will be the edit area (text field), 2nd is NULL (last element has to be NULL) 
+  FORM * form; //attatch field[0] to form to make it scrollable and gain more functionalities 
+  const char * filename;
+
+
+ public:
+  e(const char *); //constructor---takes in a filename (the file to edit)
+  void handleKeyInput(); //listen for key input and handle accordingly 
+  void openMenu();
+  void displayFile(string, int);
+
+
+  /* Should these be in this class? */
+  void save(const char *);
+  void saveAs(const char *);
+  void exit();
 };
+
 
 #endif
