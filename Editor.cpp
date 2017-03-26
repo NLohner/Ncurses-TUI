b1e57770e916b2115c138f17323d4ce51cff8dbd
Nico Lohner <nlohner1997@gmail.com>
2017-03-25 20:35:34 -0400

    commented out main and fixed one -pedantic-errors error

--- Editor.cpp
+++ Editor.cpp
@@ -111,7 +111,7 @@ void Editor::displayFile(string buff, int lineNum)
     form_driver(form, REQ_DOWN_CHAR); //go down lineNum number of lines
   
   const char * arr = buff.c_str();  //turn string that was passed in into a c_string
-  for(int i = 0; i < strlen(arr); i++)
+  for(unsigned int i = 0; i < strlen(arr); i++)
     form_driver(form, arr[i]); //add each char from the string individually
 }
 
@@ -138,10 +138,10 @@ void Editor::exit()
 
 
 
-int main(int argc, const char * argv[]) 
+/*int main(int argc, const char * argv[]) 
 {
   Editor ed = Editor("newfile.txt"); //create the editor
   ed.handleKeyInput();
   ed.exit();
   return 0;
-}
+  }*/
